c1bcbb3957d522bf3662105e1a220477af87ec53
Ahyeon Gil <ahyeonlog@gmail.com>
2021-04-27 01:14:49 +0900

    ahyeon -S: pro 괄호변환, 순위검색, 신규아이디

--- AHYEON/kakao/pro_괄호변환.py
+++ AHYEON/kakao/pro_괄호변환.py
@@ -1,19 +1,48 @@
-def solve(p):
+def check(p):
     if p == '':  # 1
         return ''
-    u, v = split(p)
-    if right(u):
-        return 
+    u, v = split(p)  # 2 두 균형잡힌 괄호 문자열로 분리, i
+    if right(u):  # 3  u가 올바른 괄호 문자열이라면 v에 대해 1단계부터 수행
+        return u + check(v)  # 3-1
+    else:  # 4
+        return '(' + check(v) + ')' + reverse(u[1:-1])
 
 
+def split(p):
+    for i in range(len(p)):
+        u = p[:i+1]
+        o = u.count('(')
+        c = u.count(')')
+        if o == c and o + c < len(p):
+            return u, p[i+1:]
+    return p, ""
 
+
+def right(p):
+    q = []
+    for i in p:
+        if i == '(':
+            q.append(i)
+        else:
+            if q:
+                q.pop()
+            else:
+                return False
+    if q: return False
+    return True
+
+
+def reverse(p):
+    r = ''
+
+    for i in p:
+        if i == '(':
+            r += ')'
+        else:
+            r += '('
+    return r
 
 
 def solution(p):
-    answer = ''
-
-
-
-
-    return solve(p)
+    return check(p)
 
