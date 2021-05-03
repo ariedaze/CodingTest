5f136641d3a9cbd9c6f9173d206c38853c249875
Ahyeon Gil <ahyeonlog@gmail.com>
2021-05-03 21:13:38 +0900

    ahyeon -S: boj 10800

--- AHYEON/GREEDY/boj_10800_컬러볼.py
+++ AHYEON/GREEDY/boj_10800_컬러볼.py
@@ -1,2 +1,28 @@
 import sys
-sys.stdin = open('input/boj_10800_컬러볼.txt', 'r')
+input = sys.stdin.readline
+
+
+N = int(input())
+
+ball = [[0, 0, i] for i in range(N)]
+ans = [0 for i in range(N)]
+
+for i in range(N):
+    ball[i][1], ball[i][0] = map(int, input().split())
+
+ball.sort()
+
+colors = [0 for i in range(N + 1)]
+s = 0
+j = 0
+
+for i in range(N):
+    while ball[j][0] < ball[i][0]:
+        colors[ball[j][1]] += ball[j][0]
+        s += ball[j][0]
+        j += 1
+
+    ans[ball[i][2]] = s - colors[ball[i][1]]
+
+for i in range(N):
+    print(ans[i])
