3a82398209d1b56a3f601f071239c9211e701756
이동규 <uykgnod@naver.com>
2021-05-27 17:11:35 +0900

    동규 -S: pro_체육복, 큰수만들기, 구명보트, 섬 연결하기 -F: 조이스틱

---
+++ 이동규/그리디 3번째/구명보트.py
@@ -0,0 +1,24 @@
+# import sys
+# sys.stdin = open('input/구명보트.txt', 'r')
+
+from collections import deque
+
+def solution(people, limit):
+    boat = 0
+    people.sort()
+
+    q = deque(people)
+    while q:
+        if len(q) >= 2:
+            if q[0] + q[-1] <= limit:
+                q.pop()
+                q.popleft()
+                boat += 1
+            elif q[0] + q[-1] > limit:
+                q.pop()
+                boat += 1
+        else:
+            if q[0] <= limit:
+                q.pop()
+                boat += 1
+    return boat
